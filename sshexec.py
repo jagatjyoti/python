23c89a00662104ac66d08aa6533f6bf19caa9083
jagat <jagatjyoti0@gmail.com>
2016-05-21 23:17:36 +0530

    ssh testing using paramiko

---
+++ sshexec.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import paramiko
+try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('ip=192.168.87.128', username='luckee', password='lucky@123')
+    stdin, stdout, stderr = ssh.exec_command('uptime')
+    print "Output: \n", stdout.readlines()
+except paramiko.SSHException:
+    print "Error: \n", stderr.readlines()
+finally:
+    ssh.close()
+    print "Active SSH connection closed"
