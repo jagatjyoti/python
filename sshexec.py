1d9e4c2fdcb6af3ffe4df22b5c18db97e4f7ff11
jagat <jagatjyoti0@gmail.com>
2016-06-05 22:52:27 +0530

    Second commit

--- sshexec.py
+++ sshexec.py
@@ -4,7 +4,7 @@ import paramiko
 try:
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('ip=192.168.87.128', username='luckee', password='lucky@123')
+    ssh.connect('ip=192.168.87.129', username='luckee', password='lucky@123')
     stdin, stdout, stderr = ssh.exec_command('uptime')
     print "Output: \n", stdout.readlines()
 except paramiko.SSHException:
