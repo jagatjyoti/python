dde911bbda0b836881bf04462a385107092a7a8b
jagat <jagatjyoti0@gmail.com>
2016-06-19 21:48:29 +0530

    Final commit

--- sshexec.py
+++ sshexec.py
@@ -4,8 +4,9 @@ import paramiko
 try:
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('192.168.87.129', username='luckee', password='lucky@123')
-    stdin, stdout, stderr = ssh.exec_command('uptime')
+    print "Trying to connect to host ..."
+    ssh.connect('192.168.87.128', username='luckee', password='user@1234')
+    stdin, stdout, stderr = ssh.exec_command('hostname')
     print "Output: \n", stdout.readlines()
 except paramiko.SSHException:
     print "Error: \n", stderr.readlines()
