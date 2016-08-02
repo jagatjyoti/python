ef29300c9691eb3ef49e7a7e0b5e5d59594079cd
jagat <jagatjyoti0@gmail.com>
2016-08-02 22:56:26 +0530

    Rectified code error

---
+++ cond.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+a = 21
+b = 10
+
+if ( a == b ) :
+   print " a is equal to b"
+else:
+   print "a is not equal to b"
+
+if ( a > b) :
+   print "a is greater than b"
+else:
+   print "b is greater than a"
+
+if ( a < b) :
+   print "a is less than b"
+else:
+   print "b is less than a"
+
+if (a != b) :
+   print "a is not equal to b"
+else:
+   print "a is equal to b"
+
+print "All comparisional operations successful !!!"
