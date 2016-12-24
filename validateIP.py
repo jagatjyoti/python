6e04e4b14f9a5a4f6dc5ddf44f6bea8c58992995
jagat <jagatjyoti0@gmail.com>
2016-12-24 12:05:29 +0530

    Not yet tested

--- validateIP.py
+++ validateIP.py
@@ -2,8 +2,23 @@
 
 address = input("Enter an IP to be validated: ")
 
-count = address.count('.')
-if count != 3:
+def validateIP():
+    count = address.count('.')
+        if count != 3:
+            return False
+        else:
+            for ele in address:
+                if not ele.isdigit():
+                    return False
+                i = int(ele)
+                if i < 0 or i > 255:
+                    return False
+    return True
+
+result = validateIP()
+
+if result = False:
     print "Entered IP is invalid. Exiting ..."
+    sys.exit(1)
 else:
-    
+    print "IP validation successful !!"
