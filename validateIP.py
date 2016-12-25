4756f8b4f800733dfd4c9eac6bb776bd6511e18a
jagat <jagatjyoti0@gmail.com>
2016-12-25 23:16:53 +0530

    Need to fix error

--- validateIP.py
+++ validateIP.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python
 
-address = input("Enter an IP to be validated: ")
+import sys
 
-def validateIP():
+address = raw_input("Enter an IP to be validated: ")
+
+def validateIP(address):
     count = address.count('.')
-        if count != 3:
+    if count != 3:
+            print "Inside count"
             return False
-        else:
+    else:
             for ele in address:
                 if not ele.isdigit():
+                    print type(ele)
+                    print "Inside isdigit"
                     return False
                 i = int(ele)
                 if i < 0 or i > 255:
+                    print "Inside numcheck"
                     return False
     return True
 
-result = validateIP()
+result = validateIP(address)
 
-if result = False:
+if result == False:
     print "Entered IP is invalid. Exiting ..."
     sys.exit(1)
 else:
