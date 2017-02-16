95d9eb835126d92851fb9c8f9ab2cae889e1255b
jagat <jagatjyoti0@gmail.com>
2017-02-16 23:07:34 +0530

    New code added

--- python_restapi.py
+++ python_restapi.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python
 
 import requests
+import json
 
 r = requests.get('https://jsonplaceholder.typicode.com/posts')
 print "Status code:", r.status_code
-print "Headers:", r.headers
-print "Content:", r.content
-
-
+#print "Headers:", r.headers
+#print "Content:", r.content
+data = json.loads(r)
+"""
 r = requests.get('https://jsonplaceholder.typicode.com/posts/1/comments')
 print "Status code:", r.status_code
 print "Headers:", r.headers
 print "Content:", r.content
+
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+data = open('gtt.json')
+r = requests.post('https://jsonplaceholder.typicode.com/posts', headers=headers, data=data)
+print "Status code:", r.status_code
+print "Headers:", r.headers
+print "Content:", r.content
+"""
